d6fa31e0e751be01e3f9912d65ec8f3a05e8d9e5
Bastian Feder <github@bastian-feder.de>
2013-07-16 08:46:12 +0200

    removed unnecessary use statements

--- Controller/DataAggregatorController.php
+++ Controller/DataAggregatorController.php
@@ -3,11 +3,6 @@
 namespace Liip\DataAggregatorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-
-// these import the "@Route" and "@Template" annotations
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DataAggregatorController extends Controller
 {
