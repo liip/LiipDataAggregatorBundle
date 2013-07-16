b351cfe99ff3c98682d2771a271c3de1f9b48d92
Bastian Feder <github@bastian-feder.de>
2013-07-16 08:43:07 +0200

    Added api docblocks, code cleanup, and minor refactoring

--- Command/DataAggregatorCommand.php
+++ Command/DataAggregatorCommand.php
@@ -4,10 +4,10 @@ namespace MigrosApi\DataAggregatorBundle\Command;
 use Assert\Assertion;
 use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManager;
-use MigrosApi\DataAggregatorBundle\DataAggregator;
-use MigrosApi\DataAggregatorBundle\Loaders\LoaderBoss;
-use MigrosApi\DataAggregatorBundle\Loaders\Entities\LoaderEntityBoss;
-use MigrosApi\DataAggregatorBundle\Persistors\PersistorBoss;
+use Liip\DataAggregatorBundle\DataAggregator;
+use Liip\DataAggregatorBundle\Loaders\Entities\LoaderEntityBoss;
+use Liip\DataAggregatorBundle\Loaders\LoaderBoss;
+use Liip\DataAggregatorBundle\Persistors\PersistorBoss;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -16,55 +16,102 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class DataAggregatorCommand extends ContainerAwareCommand
 {
+    /**
+     * @var \Liip\DataAggregatorBundle\Loaders\LoaderBoss
+     */
+    protected $loader;
+    /**
+     * @var \Liip\DataAggregatorBundle\Loaders\Entities\LoaderEntityBoss
+     */
+    protected $loaderEntityBoss;
+    /**
+     * @var \Liip\DataAggregatorBundle\Persistors\PersistorBoss
+     */
+    protected $persistor;
+
+    /**
+     * Sets up the command to be used from console.
+     */
     protected function configure()
     {
         $this
             ->setName('dataaggregator:import')
-            ->setDescription('Import from all datasources')
-        ;
+            ->setDescription('Import from all datasources');
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        echo("Setting PHP memory_limit to 2000M\n");
+        $output->write("Setting PHP memory_limit to 2000M\n");
         ini_set('memory_limit', '2000M');
 
-        echo("Fetching BOSS data from oracle database ... ");
+        $output->write("Fetching BOSS data from oracle database ... ");
 
         // get DBAL and entity manager
         $dbal = $this->getContainer()->get("doctrine")->getConnection('boss');
         $em = $this->getContainer()->get("doctrine")->getManager();
 
-        // get assertion instance
-        $assertion = new Assertion();
-
-        list ($loader, $persistor) = $this->getBossLoaderAndPersistor($assertion, $dbal, $em);
+        // get BOSS config
+        $config = $this->getContainer()->getParameter('data_aggregator.boss');
 
         // get data aggregator
         $da = new DataAggregator();
-        $da->attachLoader($loader);
-        $da->attachPersistor($persistor);
+        $da->attachLoader($this->getBossLoader($config['loader'], new Assertion(), $dbal));
+        $da->attachPersistor($this->getBossPersistor($config['persistor'], $em));
         $da->run();
 
-        echo("done!\n");
+        $output->write("done!\n");
     }
 
-    protected function getBossLoaderAndPersistor(Assertion $assertion, Connection $dbal, EntityManager $em)
+    /**
+     * @param array $config
+     * @param Assertion $assertion
+     * @param Connection $dbal
+     *
+     * @return LoaderBoss
+     */
+    protected function getBossLoader(array $config, Assertion $assertion, Connection $dbal)
     {
-        // get BOSS config
-        $bossConfig = $this->getContainer()->getParameter('data_aggregator.boss');
-        $loaderBossConfig = $bossConfig['loader'];
-        $persistorBossConfig = $bossConfig['persistor'];
+        if (empty($this->loader)) {
 
-        // get entity
-        $entity = new LoaderEntityBoss($assertion, $loaderBossConfig);
+            $this->loader = new LoaderBoss($dbal, $this->getLoaderEntityBoss($assertion, $config));
+        }
 
-        // define loader
-        $loader = new LoaderBoss($dbal, $entity);
+        return $this->loader;
+    }
 
-        // define persistor
-        $persistor = new PersistorBoss($em, $persistorBossConfig);
+    /**
+     * @param $assertion
+     * @param $config
+     *
+     * @return LoaderEntityBoss
+     */
+    protected function getLoaderEntityBoss($assertion, $config)
+    {
+        if (empty($this->loaderEntityBoss)) {
 
-        return array($loader, $persistor);
+            $this->loaderEntityBoss = new LoaderEntityBoss($assertion, $config);
+        }
+
+        return $this->loaderEntityBoss;
+    }
+
+    /**
+     * @param array $config
+     * @param EntityManager $em
+     *
+     * @return PersistorBoss
+     */
+    protected function getBossPersistor(array $config, EntityManager $em)
+    {
+        if (empty($this->persistor)) {
+
+            $this->persistor = new PersistorBoss($em, $config);
+        }
+
+        return $this->persistor;
     }
 }
